742ba1318ecc15663776d17ff8030076d232d876
Leo Dion <leogdion@brightdigit.com>
2022-06-04 20:40:11 -0400

    working on building machines

--- Bushel/MachineView.swift
+++ Bushel/MachineView.swift
@@ -55,6 +55,7 @@ struct MachineNetwork {
   }
   let type : InterfaceType
   let macAddress : MacAddress
+  
 }
 
 struct MachineSharedDirectory {
@@ -264,10 +265,10 @@ struct MachineBuilderRange {
 struct MachineBuilder {
   internal init(name: String? = nil, cpuCount: Int = 1, memorySize: UInt64 = (4 * 1024 * 1024 * 1024), displays: [MachineDisplay] = [MachineDisplay](), disks: [MachineDisk] = [MachineDisk](), shares: [MachineSharedDirectory] = [MachineSharedDirectory](), sourceImage: LocalImage,useHostAudio : Bool = true) {
     
-    self.cpuCount = cpuCount
-    self.memorySize = memorySize
-    self.displays = displays
-    self.disks = disks
+//    self.cpuCount = cpuCount
+//    self.memorySize = memorySize
+//    self.displays = displays
+//    self.disks = disks
     self.sourceImage = sourceImage
     self.name = name ?? sourceImage.name
     self.useHostAudio = useHostAudio
@@ -276,9 +277,9 @@ struct MachineBuilder {
   var name : String
   var cpuCount : Int = 1
   var memorySize : UInt64 = (4 * 1024 * 1024 * 1024)
-  var displays = [MachineDisplay]()
+  var displays = [MachineDisplay(width: 1920, height: 1080, pixelsPerInch: 76)]
   var disks = [MachineDisk(size: 64 * 1024 * 1024 * 1024)]
-  var networks = [MachineNetwork]()
+  var networks = [MachineNetwork(type: .NAT, macAddress: .random)]
   var shares = [MachineSharedDirectory]()
   let sourceImage : LocalImage
   let useHostAudio : Bool
