d797e1bbb4f2256b3af2e77b2561a0b5dde44697
Leo Dion <leogdion@brightdigit.com>
2022-06-08 21:50:24 -0400

    Adding icons and design view

---
+++ BshIllDesign/BshIllDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  BshIllDesignApp.swift
+//  BshIllDesign
+//
+//  Created by Leo Dion on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct BshIllDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
