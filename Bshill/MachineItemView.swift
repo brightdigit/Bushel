c93041afa4099e09890fc06f1e12aacab5b9bf43
Leo Dion <leogdion@brightdigit.com>
2022-06-18 09:47:20 -0400

    trying a new design

--- Bshill/MachineItemView.swift
+++ Bshill/MachineItemView.swift
@@ -7,9 +7,51 @@
 
 import SwiftUI
 
+
+struct MachineSharedDirectory : Identifiable {
+    internal init(url: URL, tag: String, isReadOnly: Bool = false) {
+        self.url = url
+        self.tag = tag
+        self.isReadOnly = isReadOnly
+    }
+    
+  let url : URL
+  let tag : String
+  let isReadOnly : Bool
+    
+    var id : String {
+        return self.tag
+    }
+}
+
+struct MachineDisk : Identifiable, Hashable {
+  let id : UUID
+    let name : String
+  let size: UInt64
+  let readOnly : Bool
+    init(id: UUID = .init(), name: String, size: UInt64, readOnly : Bool = false) {
+    self.id = id
+        self.name = name
+    self.size = size
+    self.readOnly = readOnly
+  }
+}
+
 struct MachineItemView: View {
+    @State var selectedDisk : Set<MachineDisk.ID> = .init()
+    @State var selectedSharedDirectory : Set<MachineSharedDirectory.ID> = .init()
     @State var shouldDisplayIconPicker = false
     @State var machineIcon : ImageItem = .init(name: "085-lisa")
+    
+    @State var disks : [MachineDisk] = [
+        .init(name: "Hello", size: 16000000000),
+        .init(name: "Hello", size: 16500000000)
+    ]
+    
+    
+    @State var sharedDirectories : [MachineSharedDirectory] = [
+        .init(url: URL(fileURLWithPath: "/home/leo/Documents", isDirectory: true), tag: "Documents")
+    ]
 //    var name : String
 //    var cpuCount : Int = 1
 //    var memorySize : UInt64 = (4 * 1024 * 1024 * 1024)
@@ -77,9 +119,12 @@ struct MachineItemView: View {
                 
                 LabeledContent {
                     VStack(spacing: 0.0){
-                        List{
-                            
-                        }
+                        Table(self.disks, selection: self.$selectedDisk) {
+                            TableColumn("Name", value: \.name)
+                            TableColumn("Size") { (disk : MachineDisk) in
+                                Text("\(ByteCountFormatter().string(from: .init(value: .init(disk.size), unit: .bytes)))")
+                            }
+                        }.frame(height: 120.0)
                         HStack(spacing: 0.0){
                             Button {
                                 
@@ -101,9 +146,14 @@ struct MachineItemView: View {
                 }
                 
                 LabeledContent {
-                    List{
-                        
-                    }
+                    
+                    Table(self.sharedDirectories, selection: self.$selectedSharedDirectory) {
+                            TableColumn("Tag", value: \.tag)
+                        TableColumn("Path", value: \.url.relativePath)
+                        TableColumn("Read Only", value: \.isReadOnly) { (share : MachineSharedDirectory) in
+                            Image(systemName: "lock.fill")
+                        }
+                        }.frame(height: 120.0)
                 } label : {
                     Text("Shared Locations")
                 }
