98e219bec7e15e8b091fe50baf4ac34bf6a74553
Leo Dion <leogdion@brightdigit.com>
2022-07-25 20:15:34 -0400

    removing old project targets

--- Bushel/ClosedRange.swift
+++
@@ -1,7 +0,0 @@
-
-
-extension ClosedRange {
-    init<Other: Comparable>(_ other: ClosedRange<Other>, _ transform: (Other) -> Bound) {
-        self = transform(other.lowerBound)...transform(other.upperBound)
-    }
-}
